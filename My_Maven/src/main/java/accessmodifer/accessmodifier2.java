3eb9443f421fe152d2a68864d41e5c6152689c0c
urmyl <urmyl@svplap0017>
2024-01-18 15:29:30 +0530

    First Commit

---
+++ My_Maven/src/main/java/accessmodifer/accessmodifier2.java
@@ -0,0 +1,5 @@
+package accessmodifer;
+
+public class accessmodifier2 {
+
+}
---
+++ Seleniumproject/src/main/java/Collection/CollectionSample.java
@@ -0,0 +1,47 @@
+package Collection;
+
+/*import java.util.ArrayList;
+
+public class CollectionSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayList a1 = new ArrayList();
+		a1.add("Hello");
+		
+		a1.add(2);
+		a1.add(2.5);
+		a1.add(3);
+		
+		System.out.println(a1);
+		
+		ArrayList a2 = new ArrayList<String>(); // generic array list
+		a2.add("World");
+		a2.add("Good");
+		a2.add("hi");
+		System.out.println(a2);
+		a2.remove(2);
+		System.out.println(a2);
+	
+	//add all
+	ArrayList<String>a3=new ArrayList();
+	a3.add("Apple");
+	a3.add("Mango");
+	
+	a2.addAll(a3);
+	System.out.println(a2);
+	
+	//removeAll
+	
+	a1.removeAll(a1);
+	System.out.println(a1);
+	}
+	
+	
+	//get
+	
+	System.out.println(a2.get(0));
+	
+}
+*/
